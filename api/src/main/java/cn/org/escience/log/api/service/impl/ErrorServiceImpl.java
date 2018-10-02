56844af8775528a6da27c7f97b26780f3527a695
zzq <191550636@qq.com>
2018-10-02 12:56:50 +0800

    增加错误类型后增加对错误类型的查询接口

--- api/src/main/java/cn/org/escience/log/api/service/impl/ErrorServiceImpl.java
+++ api/src/main/java/cn/org/escience/log/api/service/impl/ErrorServiceImpl.java
@@ -1,8 +1,14 @@
 package cn.org.escience.log.api.service.impl;
 
+import cn.org.escience.log.api.mapper.Errors400Mapper;
+import cn.org.escience.log.api.mapper.Errors403Mapper;
 import cn.org.escience.log.api.mapper.Errors404Mapper;
 import cn.org.escience.log.api.mapper.ErrorsMapper;
 import cn.org.escience.log.api.model.Errors;
+import cn.org.escience.log.api.model.Errors400;
+import cn.org.escience.log.api.model.Errors400Example;
+import cn.org.escience.log.api.model.Errors403;
+import cn.org.escience.log.api.model.Errors403Example;
 import cn.org.escience.log.api.model.Errors404;
 import cn.org.escience.log.api.model.Errors404Example;
 import cn.org.escience.log.api.model.ErrorsExample;
@@ -18,6 +24,8 @@ public class ErrorServiceImpl extends BaseService implements ErrorService {
 
   private ErrorsMapper errorsMapper = sqlSession.getMapper(ErrorsMapper.class);
   private Errors404Mapper errors404Mapper = sqlSession.getMapper(Errors404Mapper.class);
+  private Errors400Mapper errors400Mapper = sqlSession.getMapper(Errors400Mapper.class);
+  private Errors403Mapper errors403Mapper = sqlSession.getMapper(Errors403Mapper.class);
 
   public ErrorServiceImpl(String database) {
     super(database);
@@ -56,8 +64,20 @@ public class ErrorServiceImpl extends BaseService implements ErrorService {
         edvo.setPage(pageInfo);
         break;
       case 403:
+        Errors403Example errors403Example = new Errors403Example();
+        errors403Example.createCriteria().andYearMonthIn(dates);
+        errors403Example.setOrderByClause("hits DESC");
+        List<Errors403> errors403s = errors403Mapper.selectByExample(errors403Example);
+        PageInfo<Errors403> e403page = new PageInfo<>(errors403s);
+        edvo.setPage(e403page);
         break;
       case 400:
+        Errors400Example errors400Example = new Errors400Example();
+        errors400Example.createCriteria().andYearMonthIn(dates);
+        errors400Example.setOrderByClause("hits DESC");
+        List<Errors400> errors400s = errors400Mapper.selectByExample(errors400Example);
+        PageInfo<Errors400> e400page = new PageInfo<>(errors400s);
+        edvo.setPage(e400page);
         break;
     }
 
